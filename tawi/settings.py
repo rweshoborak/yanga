1593783fd93720061b751e21a3352d5231abedbe
rweshoborak <rweshoborakelvin@ymail.com>
2023-05-10 08:13:20 +0300

    Added requirments.txt file, configured mysql database and migrated the changes

--- tawi/settings.py
+++ tawi/settings.py
@@ -60,10 +60,15 @@ WSGI_APPLICATION = "tawi.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yanga',
+        'USER': 'kevo',
+        'PASSWORD': '9@Kagemulo9',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
